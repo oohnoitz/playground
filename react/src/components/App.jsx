b9cb30fa28b78324e26a8b2c5767410aaf50f153
Hung Tran <hung@0x0.ninja>
2017-10-31 18:23:24 -0500

    implement error boundary component

--- react/src/components/App.jsx
+++ react/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ErrorBoundary from './ErrorBoundary'
 import Nav from './Nav'
 
 export default class App extends Component {
@@ -6,6 +7,9 @@ export default class App extends Component {
     return (
       <div className='mdl-layout mdl-js-layout mdl-layout--fixed-header'>
         <Nav/>
+        <ErrorBoundary>
+
+        </ErrorBoundary>
       </div>
     )
   }
