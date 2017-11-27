ebabdf58917335e7e111a17b5c1b4acb0efe281a
Hung Tran <hung@0x0.ninja>
2017-11-26 20:25:03 -0600

    wip

--- react/src/components/App.jsx
+++ react/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ErrorBoundary from './ErrorBoundary'
 import Nav from './Nav'
+import Content from './Content'
 
 export default class App extends Component {
   render() {
@@ -8,7 +9,7 @@ export default class App extends Component {
       <div className='mdl-layout mdl-js-layout mdl-layout--fixed-header'>
         <Nav/>
         <ErrorBoundary>
-
+          <Content/>
         </ErrorBoundary>
       </div>
     )
